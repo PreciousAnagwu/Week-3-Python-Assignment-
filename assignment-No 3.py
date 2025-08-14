db3606efd99527e40a804e82103aa65d993c7a7e
PreciousAnagwu <panagwu@gmail.com>
2025-08-14 13:50:38 +0100

    Comments

--- assignment-No 3.py
+++ assignment-No 3.py
@@ -1,3 +1,4 @@
+#No1 - creating my function
 def calculate_discount(price, discount_percent):
     if discount_percent >= 20:
         discount_amount = (discount_percent/100) * price
@@ -6,9 +7,9 @@ def calculate_discount(price, discount_percent):
     else:
         return price
 
-#No2- Get the user input
-original_price= float(input("Enter original price of item:"))
-discount_percent = float(input("Enter discount percentage:"))
+#No2 - Get the user input
+original_price= float(input("Enter original price of item:"))    #i used float so it can handle decimal numbers 
+discount_percent = float(input("Enter discount percentage:"))    #i used float so it can handle decimal numbers 
 
 final_price= calculate_discount(original_price, discount_percent)
 print("The final price of this item is:", final_price)
